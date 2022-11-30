52f5011c350b91e15508b58e0f78ad40169db9d0
muchamadkurniawan <kernel.journal@itats.ac.id>
2022-11-30 09:08:23 +0700

    add kfold

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn import svm
 from sklearn import tree
@@ -172,6 +173,30 @@ class class_adaboostSVM:
         # print(len(self.X))
         svc=SVC(probability=True, kernel='rbf')
         self.model =AdaBoostClassifier(n_estimators=50, base_estimator=svc,learning_rate=1)
+        self.model.fit(np.array(self.X), self.y)
+
+    def predict(self, data_testing):
+        # print("prediksi : ", self.model.predict(data_testing))
+        # print(data_testing)
+        return self.model.predict(data_testing)
+class classLinearRegression:
+    X = []
+    y = []
+    model = None
+
+    def __init__(self, data, label):
+        print("Linear Regression")
+        self.X = data
+        self.y = label
+
+    def viewDataset(self):
+        print("data :", self.X)
+        print("label :", self.y)
+
+    def model(self):
+        # print("in model X :", self.X)
+        # print(len(self.X))
+        self.model=LogisticRegression()
         self.model.fit(np.array(self.X), self.y)
 
     def predict(self, data_testing):
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from classifier import *
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from feature_selection import *
+from sklearn.model_selection import KFold
+import statistics
 
 import numpy as np
 if __name__ == '__main__':
@@ -13,7 +15,7 @@ if __name__ == '__main__':
     # glcm
     # dissimilarity
     # asmaDataset
-    data = class_dataset(dataset_name="asmaDataset")
+    data = class_dataset(dataset_name="glcm")
     # print("data freme :\n", data.df)
     # print("feature :\n", data.X)
     # print("class :\n", data.y)
@@ -22,74 +24,109 @@ if __name__ == '__main__':
 
 
     #preprocesing (normalisasi)
-    select = feature_selection(data.X, data.y)
-    # data.X = select.linearSVC()
-    # data.X = select.tree_based()
-    # data.X = select.pearson_correletion(0.35)
-    # data.X = select.selectKbest_Anova(4)
-    # data.X = select.selectKbest_Chi2(4)
-    # data.X = select.selectKbest_regression(4)
-    data.X = select.RFE_SVC(4)
+    # select = feature_selection(data.X, data.y)
+    # # data.X = select.linearSVC()
+    # # data.X = select.tree_based()
+    # # data.X = select.pearson_correletion(0.35)
+    # # data.X = select.selectKbest_Anova(4)
+    # # data.X = select.selectKbest_Chi2(4)
+    # # data.X = select.selectKbest_regression(4)
+    # data.X = select.RFE_SVC(4)
 
     # random split dataset
     X_train, X_test, y_train, y_test = train_test_split(
         data.X, data.y, test_size=0.33, random_state=42)
-
-    # model KNN
-    print("--------------------------------")
-    knn = class_KNN(X_train, y_train)
-    knn.model()
-    y_pred = knn.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy : ",acc)
-
-    ## model NB
-    print("--------------------------------")
-    nb = class_NB(X_train, y_train)
-    nb.model()
-    y_pred = nb.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ",acc)
-
-    ##  Model Random Forest
-    print("--------------------------------")
-    RF = class_RandomForest(X_train, y_train)
-    RF.model()
-    y_pred = RF.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ", acc)
-
-    ## Model Decesion Tree
-    print("--------------------------------")
     DT = class_DecisionTree(X_train, y_train)
     DT.model()
     y_pred = DT.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ", acc)
+    accRSD = accuracy_score(y_test, y_pred)
 
-    ## model SVM
-    print("--------------------------------")
-    SVM = class_SVM(X_train, y_train)
-    SVM.model()
-    y_pred = SVM.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ", acc)
 
-    ## model MLP
-    print("--------------------------------")
-    mlp = class_MLP(X_train, y_train)
-    mlp.model()
-    y_pred = mlp.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ", acc)
+    # Kfold cross validation
+    # kfold with NB
+    data.y = np.array(data.y)
+    # print(data.y)
+    kf = KFold(n_splits=3, random_state=None, shuffle=True)
+    accAll = []
+    for train_index, test_index in kf.split(data.X):
+        print("train index : ", train_index)
+        # print("test index :",test_index)
+        X_train, X_test = data.X[train_index,:], data.X[test_index,:]
+        # print("xtrain ", X_train)
+        # print("xtest ", X_test)
+        y_train, y_test = data.y[train_index], data.y[test_index]
+        # print("len train :", len(y_train))
+        # print("len test :",len(y_test))
+        # model KNN
+        print("--------------------------------")
+        DT = class_DecisionTree(X_train, y_train)
+        DT.model()
+        y_pred = DT.predict(X_test)
+        # print("compare :", y_pred, y_test)
+        acc = accuracy_score(y_test, y_pred)
+        print("accuracy: ", acc)
+        # print("accuracy : ", acc)
+        accAll.append(acc)
+    print("akurasi rata-rata (Kfold) : ", statistics.mean(accAll))
+    print("ACC Random split dataset: ", accRSD)
 
-    ## model Adaboost - MLP
-    print("--------------------------------")
-    adaboost = class_adaboostSVM(X_train, y_train)
-    adaboost.model()
-    y_pred = adaboost.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print("accuracy: ", acc)
+    #
+
+    # ## model NB
+    # print("--------------------------------")
+    # nb = class_NB(X_train, y_train)
+    # nb.model()
+    # y_pred = nb.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ",acc)
+    #
+    # ##  Model Random Forest
+    # print("--------------------------------")
+    # RF = class_RandomForest(X_train, y_train)
+    # RF.model()
+    # y_pred = RF.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
+    #
+    # ## Model Decesion Tree
+    # print("--------------------------------")
+    # DT = class_DecisionTree(X_train, y_train)
+    # DT.model()
+    # y_pred = DT.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
+    #
+    # ## model SVM
+    # print("--------------------------------")
+    # SVM = class_SVM(X_train, y_train)
+    # SVM.model()
+    # y_pred = SVM.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
+    #
+    # ## model MLP
+    # print("--------------------------------")
+    # mlp = class_MLP(X_train, y_train)
+    # mlp.model()
+    # y_pred = mlp.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
+    #
+    # ## model Adaboost - MLP
+    # print("--------------------------------")
+    # adaboost = class_adaboostSVM(X_train, y_train)
+    # adaboost.model()
+    # y_pred = adaboost.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
+    #
+    # ## Linear Regression
+    # print("--------------------------------")
+    # LR = classLinearRegression(X_train, y_train)
+    # LR.model()
+    # y_pred = LR.predict(X_test)
+    # acc = accuracy_score(y_test, y_pred)
+    # print("accuracy: ", acc)
 
     # # #evaluasi
     # #
